5b1ff7f189bd12204ac0dcd15ce916013443ae7c
Антон <antoinedzejob@gmail.com>
2019-12-11 20:22:56 +0300

    react practice

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 const path = require('path');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	module: {
-	    rules: [
-	      	{
-	        	test: /\.ejs$/,
-	        	use: ['ejs-webpack-loader']
-	    	}
-		]
-	}
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                process.env.NODE_ENV === 'production' ? '@babel/preset-env' : '', 
+                '@babel/preset-react'
+              ].filter(v => v)
+            }
+          }
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs'
+    })
+  ]
 }
