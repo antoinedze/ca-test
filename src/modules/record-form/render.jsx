eac68f2170484e56380b8970bbe34be7d5f36eaa
Антон <antoinedzejob@gmail.com>
2019-12-12 19:46:53 +0300

    render

--- src/modules/record-form/render.jsx
+++ src/modules/record-form/render.jsx
@@ -1,3 +1,10 @@
 const React = require('react');
 
-module.exports = void(0);
+module.exports = ({Form, Input, SubmitButton}, props) => (
+  <Form className="button-wrapper">
+    <div className="input-wrapper">
+      <Input/>
+    </div>
+    <SubmitButton><span className="button-inner">{props.submitText}</span></SubmitButton>
+  </Form>
+)
